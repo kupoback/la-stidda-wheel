4e4f656f3fd9dc676489087ecaae49e2bb58fd81
Nick Makris <nmakris@cliquestudios.com>
2025-02-13 11:29:35 -0500

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<script>
+    export default {
+        name: 'Wheel of Fotune',
+    };
+</script>
+
+<template>
+    <div class="text-gray-900 bg-gray-100 font-serif dark:bg-gray-800 dark:text-white h-max">
+        <main>
+            <h1>La Stidda Wheel</h1>
+        </main>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
